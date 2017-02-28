c8deefe1022955b620ff496af0ac8caf04ac91bc
Katie <katiearina@gmail.com>
2017-02-28 18:21:37 -0500

    work done last night.

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var bookTitle, currentGuess, bookQuote, previousGuess, guessRemaining
+
+// List of Book Titles
+var bookTitle = ["Kafka on the Shore", "The Wind-Up Bird Chronicle",
+"Norwegian Wood", "A Wild Sheep Chase", "Sputnik Sweetheart", 
+"Hard-Boiled Wonderland and the End of the World", "Dance Dance Dance",
+"South of the Border, West of the Sun", "The Elephant Vanishes",
+"Hear the Wind Sing"];
+
+// List of Quotes for each Book Title
+
